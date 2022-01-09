41c5e30fe567be2630e6d4827ab0d152ca0a6760
Ileenfdz <ileenfdz@gmail.com>
2022-01-09 13:23:10 +0100

    Added new method, charCode

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 // import { appendChild } from './modules/appendChild.js';
-// import { charAt } from './modules/charAt.js';
-// import {charCodeAt} from './modules/charCodeAt.js';
+// import { charAt } from './modules/char/charAt.js';
+// import {charCodeAt} from './modules/char/charCodeAt.js';
 // import {filter} from './modules/filter.js';
 // import {find} from './modules/find.js';
 // import { splitStr } from './modules/split.js';
-// import { makeUpperCase } from './modules/toUpperCase.js';
-// import { makeLowerCase } from './modules/toLowerCase.js';
+// import { makeUpperCase } from './modules/case/toUpperCase.js';
+// import { makeLowerCase } from './modules/case/toLowerCase.js';
 // import { reduce } from './modules/reduce.js';
+import { charCode } from './modules/char/charCode.js';
 
 export function app(){
     // console.log("Connected to app");
@@ -19,5 +20,6 @@ export function app(){
     // makeUpperCase();
     // makeLowerCase();
     // reduce();
+    charCode()
 }
 app();
---
+++ scripts/modules/char/charCode.js
@@ -0,0 +1,10 @@
+//It converts a string to it's ASCII equivalent in an array
+export function charCode(){
+    //from decimal to text
+    console.log(String.fromCharCode(65, 66, 67));// ABC
+    //from hex to text
+    console.log(String.fromCharCode(0x21)); //  !
+    //from binary to text 
+    console.log(String.fromCharCode(0b01001010)); // J 
+}
+
